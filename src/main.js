5737b75ce3b23d9dcfed2e83be7fed5da5254a0a
Talha-M-Ehb <talha.muhammad@student.ehb.be>
2026-05-18 17:45:12 +0200

    Vereiste documenten aangemaakt en stukje css en html al gemaakt.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './css/style.css';
+import './css/cards.css';
+import './css/modal.css';
+import './css/responsive.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DragonDex App Initialized');
+  // Further initialization will go here
+});
